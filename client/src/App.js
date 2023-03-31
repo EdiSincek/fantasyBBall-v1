5544f8aafdd849a35a7e299f7517fbe422919abf
edi.sincek <edi.sincek@ibmix.hr>
2023-03-31 15:41:31 +0200

    Cleaning main

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,11 @@
 import './App.css';
-import { useEffect } from 'react';
 import Header from './components/header/Header';
-import TeamTable from './components/teamTable/TeamTable';
-import PlayerStats from './components/playerStats/PlayerStats';
 
 function App() {
-  // useEffect(() => {
-  //   fetch("http://localhost:3001/api").then(res => console.log(res));
-  // })
 
   return (
     <div className="App">
       <Header></Header>
-      <TeamTable />
-      <PlayerStats
-        firstName="LeBron"
-        lastName="James"
-        fgPercentage="51.0%"
-        ftPercentage="73.1%"
-        threePointersMade="1.6"
-        points="25.6"
-        assists="7.9"
-        rebounds="7.9"
-        steals="1.2"
-        turnovers="3.7"
-        imageUrl="https://s3media.247sports.com/Uploads/Assets/548/529/9529548.jpg"
-      />
 
     </div>
   );

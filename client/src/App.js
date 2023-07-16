0d4baba66b1115745c93db03ec0ec48dea3a0bf4
edi.sincek <edi.sincek@ibmix.hr>
2023-07-16 13:14:37 +0200

    "adding routing"

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import Header from './components/header/Header';
-import LuckCalculator from './components/luckCalculator/LuckCalculator';
-import TeamStats from './components/team/teamStats/TeamStats'
+import HomePage from './pages/HomePage';
+import TeamPage from './pages/TeamPage'
+import { Route, Routes, Link } from 'react-router-dom'
+import AllTeamsPage from './pages/AllTeamsPage';
 
 
 function App() {
 
   return (
     <div className="App">
-      <Header></Header>
-      <LuckCalculator></LuckCalculator>
-    </div>
+      <>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><Link to="/" class="nav-link"><b>Fantasy VZ</b> </Link></li>
+            <li class="nav-item"><Link to="/allteams" class="nav-link">All Teams </Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/team/:teamId" element={<TeamPage />} />
+          <Route path="/allteams" element={<AllTeamsPage />} />
+        </Routes>
+      </>
+    </div >
   );
 }
 
---
+++ client/src/pages/AllTeamsPage.js
@@ -0,0 +1,37 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+function AllTeamsPage() {
+    return (
+        <div className="AllTeams">
+            <h1>All Teams:</h1>
+            <Link to="/team/1" > Team 1</Link>
+            <br />
+            <Link to="/team/2" > Team 2</Link>
+            <br />
+            <Link to="/team/3" > Team 3</Link>
+            <br />
+            <Link to="/team/4" > Team 4</Link>
+            <br />
+            <Link to="/team/5" > Team 5</Link>
+            <br />
+            <Link to="/team/6" > Team 6</Link>
+            <br />
+            <Link to="/team/7" > Team 7</Link>
+            <br />
+            <Link to="/team/8" > Team 8</Link>
+            <br />
+            <Link to="/team/9" > Team 9</Link>
+            <br />
+            <Link to="/team/10" > Team 10</Link>
+            <br />
+            <Link to="/team/11" > Team 11</Link>
+            <br />
+            <Link to="/team/12" > Team 12</Link>
+            <br />
+
+        </div>
+
+    )
+
+}
+export default AllTeamsPage;

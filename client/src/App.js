0c9969a940b14504de0e73441a381c24c29d8e46
edi.sincek <edi.sincek@ibmix.hr>
2023-07-16 13:53:35 +0200

    "adding matchups page"

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import TeamPage from './pages/TeamPage'
-import { Route, Routes, Link } from 'react-router-dom'
 import AllTeamsPage from './pages/AllTeamsPage';
+import MatchupsPage from './pages/MatchupsPage';
 
 
 function App() {
@@ -14,12 +15,14 @@ function App() {
           <ul class="navbar-nav mr-auto">
             <li class="nav-item"><Link to="/" class="nav-link"><b>Fantasy VZ</b> </Link></li>
             <li class="nav-item"><Link to="/allteams" class="nav-link">All Teams </Link></li>
+            <li class="nav-item"><Link to="/matchups" class="nav-link">Matchups </Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/team/:teamId" element={<TeamPage />} />
           <Route path="/allteams" element={<AllTeamsPage />} />
+          <Route path="/matchups" element={<MatchupsPage />} />
         </Routes>
       </>
     </div >
